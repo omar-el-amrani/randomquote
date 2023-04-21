8c9f1ee2c57d406c78c2b66c2a789297d661df67
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-04-21 00:55:29 +0000

    last update

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      value:"Edit <code>src/App.js</code> and save to reload."
+    };
+  }
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <div  id="quote-box">
-         <div id="text"></div>
+         <div id="text">
+          <p >
+            {this.state.value}
+          </p>
+          </div>
          <div id="author"></div>
          <button id="new-quote"></button>
-         <a id="tweet-quote" href='' ></a>
-          <p >
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+         <a id="tweet-quote" href='#' ></a>
+          
+         
         </div>
       </header>
     </div>
-  );
+    )
+  }
 }
-
-export default App;
